daf403c34df3816c89279f7e3058889028d4f4a0
kate_utkate <kate_utkate@vk.com>
2025-04-06 18:33:35 +0300

    add basic urls

--- women/views.py
+++ women/views.py
@@ -1,3 +1,11 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+
+# request = HttpRequest
+def index(request):
+    return HttpResponse('Main page of application.')
+
+
+def categories(request):
+    return HttpResponse('<h1>Categories</h1>')
