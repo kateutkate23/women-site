214d94b5813bd0f8a9395aa7ea38bbc7e3274441
kate_utkate <kate_utkate@vk.com>
2025-05-14 20:37:22 +0300

    completed admin panel

--- women/admin.py
+++ women/admin.py
@@ -1,17 +1,56 @@
 from django.contrib import admin
+from django.core.checks import messages
+
 from .models import Women, Category
+
+
+class MarriedFilter(admin.SimpleListFilter):
+    title = 'Статус женщины'
+    parameter_name = 'status'
+
+    def lookups(self, request, model_admin):
+        return [
+            ('married', 'Замужем'),
+            ('single', 'Не замужем')
+        ]
+
+    def queryset(self, request, queryset):
+        if self.value() == 'married':
+            return queryset.filter(husband__isnull=False)
+        elif self.value() == 'single':
+            return queryset.filter(husband__isnull=True)
 
 
 @admin.register(Women)
 class WomenAdmin(admin.ModelAdmin):
-    list_display = ('id', 'title', 'created_at', 'is_published', 'category')
+    list_display = ('id', 'title', 'created_at', 'is_published', 'category', 'brief_info')
     list_display_links = ('id', 'title')
-    list_editable = ('is_published', )
+    list_editable = ('is_published',)
     list_per_page = 10
+    list_filter = (MarriedFilter, 'category__name', 'is_published')
     ordering = ('created_at', 'title')
+    actions = ('publish', 'unpublish')
+    search_fields = ('title', 'category__name')
 
+    fields = ('title', 'slug', 'content', 'category', 'husband', 'tags')
+    prepopulated_fields = {"slug": ("title",)}
 
-# admin.site.register(Women, WomenAdmin)
+    filter_horizontal = ('tags',)
+
+    @admin.display(description='Краткое описание')
+    def brief_info(self, women: Women):
+        return f"Статья содержит символов: {len(women.content)}"
+
+    @admin.action(description='Опубликовать выбранные записи')
+    def publish(self, request, queryset):
+        count = queryset.update(is_published=Women.Status.PUBLISHED)
+        self.message_user(request, f"Было опубликовано записей: {count}")
+
+    @admin.action(description="Снять с публикации выбранные записи")
+    def unpublish(self, request, queryset):
+        count = queryset.update(is_published=Women.Status.DRAFT)
+        self.message_user(request, f"Снято с публикации записей: {count}!", messages.WARNING)
+
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
--- women/models.py
+++ women/models.py
@@ -2,6 +2,16 @@ from django.db import models
 from django.urls import reverse
 
 
+# def translit_to_eng(s: str) -> str:
+#     d = {'а': 'a', 'б': 'b', 'в': 'v', 'г': 'g', 'д': 'd',
+#          'е': 'e', 'ё': 'yo', 'ж': 'zh', 'з': 'z', 'и': 'i', 'к': 'k',
+#          'л': 'l', 'м': 'm', 'н': 'n', 'о': 'o', 'п': 'p', 'р': 'r',
+#          'с': 's', 'т': 't', 'у': 'u', 'ф': 'f', 'х': 'h', 'ц': 'c', 'ч': 'ch',
+#          'ш': 'sh', 'щ': 'shch', 'ь': '', 'ы': 'y', 'ъ': '', 'э': 'r', 'ю': 'yu', 'я': 'ya'}
+#
+#     return "".join(map(lambda x: d[x] if d.get(x, False) else x, s.lower()))
+
+
 class PublishedManager(models.Manager):
     def get_queryset(self):
         return super().get_queryset().filter(is_published=Women.Status.PUBLISHED)
@@ -32,6 +42,10 @@ class Women(models.Model):
 
     def get_absolute_url(self):
         return reverse('post', kwargs={'post_slug': self.slug})
+
+    # def save(self, *args, **kwargs):
+    #     self.slug = translit_to_eng(self.title)
+    #     super().save(*args, **kwargs)
 
     class Meta:
         verbose_name = "Известные женщины"
